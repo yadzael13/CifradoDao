b6228b6aa970755b6c81fbc173df17154153de6c
HIRAM YADZAEL VARGAS CHALICO <hvargasc@totalplay.corp>
2022-11-23 12:00:24 -0600

    first squeleton

---
+++ src/main/java/com/example/CifradoDao/CifradoDaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CifradoDao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifradoDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CifradoDaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CifradoDao/CifradoDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CifradoDao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifradoDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
